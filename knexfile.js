9497664d493408fc49713326e9f9facf8b93893f
Fabio Colella <fcole90@gmail.com>
2021-03-27 20:03:47 +0200

    Fixes for Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 
+const databaseFilename = process.env.DATABASE_FILENAME ?? 'database.sqlite';
+
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: process.env.DATABASE_FILENAME,
+    filename: databaseFilename,
   },
   useNullAsDefault: true,
+  migrations: {
+    directory: `${__dirname}/migrations`,
+  },
   seeds: {
-    directory: './seeds',
+    directory: `${__dirname}/seeds`,
   },
 };
